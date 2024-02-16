3b4a99f0652e413f5e3f89a6bd97548c6365eb52
LiudmylaHorobets <horobets1323@gmail.com>
2024-02-16 13:34:09 +0200

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-export const App = () => {
-  return <div>React template</div>;
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import Layout from "./components/Layout/Layout";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from 'App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/car-rental-app">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
