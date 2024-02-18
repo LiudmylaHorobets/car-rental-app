020ddf514b28f217cbd719e116788c246f8730b4
LiudmylaHorobets <127691893+LiudmylaHorobets@users.noreply.github.com>
2024-02-18 17:15:47 +0000

    Deploying to gh-pages from @ LiudmylaHorobets/car-rental-app@1ba6df4d4139ef4bebb655312867067bb869001b 🚀

--- static/js/697.1eaa7de9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rental_app=self.webpackChunkcar_rental_app||[]).push([[697],{697:function(e,a,r){r.r(a);var n=r(184);a.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:"FavoritesPage"})})}}}]);
-//# sourceMappingURL=697.1eaa7de9.chunk.js.map
