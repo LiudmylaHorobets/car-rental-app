96124e0908c0359a96e027ee39f96e08d0348e90
Liudmyla Horobets <horobets1323@gmail.com>
2024-02-18 14:35:59 +0200

    fixed styled catalog

--- src/components/CatalogList/CatalogList.styled.js
+++ src/components/CatalogList/CatalogList.styled.js
@@ -2,13 +2,10 @@ import styled from "styled-components";
 
 export const CatalogListStyled = styled.div`
   .catalog-list {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 274px);
-    grid-gap: 50px 29px;
-    grid-auto-rows: 426px;
-
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 50px 29px;
     margin-bottom: 100px;
-    margin-left: auto;
-    margin-right: auto;
   }
 `;
--- src/pages/CatalogPage/CatalogPage.styled.js
+++ src/pages/CatalogPage/CatalogPage.styled.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const CatalogWrapper = styled.section``;
+export const CatalogWrapper = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  padding: 40px 20px;
+`;
